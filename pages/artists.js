ce02207882ccf27782cb3cd05435c63ba9a1b277
An X Bui <bxan954@gmail.com>
2022-02-01 16:13:53 -0500

    Fix import

--- pages/artists.js
+++ pages/artists.js
@@ -1,6 +1,6 @@
 import Newsletter from "../components/newsletter/Newsletter";
 import Footer from "../components/footer/Footer";
-import ArtistList from "../components/artist/artistList";
+import ArtistList from "../components/artist/ArtistList";
 const Artists = () => {
   return (
     <div>
