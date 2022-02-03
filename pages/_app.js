7f9de712dfbc0efe0ae2c57447ee083f2987fdc2
An X Bui <bxan954@gmail.com>
2022-02-03 13:33:25 -0500

    Testing fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,28 @@
 import "../styles/globals.scss";
 import Header from "../components/Header";
 import { AnimatePresence } from "framer-motion";
+import Router from "next/router";
+
+const routeChange = () => {
+  // Temporary fix to avoid flash of unstyled content
+  // during route transitions. Keep an eye on this
+  // issue and remove this code when resolved:
+  // https://github.com/vercel/next.js/issues/17464
+
+  const tempFix = () => {
+    const allStyleElems = document.querySelectorAll('style[media="x"]');
+    allStyleElems.forEach((elem) => {
+      elem.removeAttribute("media");
+    });
+  };
+  tempFix();
+};
 
 function MyApp({ Component, pageProps, router }) {
   const url = router.route;
+
+  Router.events.on("routeChangeComplete", routeChange);
+  Router.events.on("routeChangeStart", routeChange);
   return (
     <>
       <Header location={url} />
